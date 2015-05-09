443357fe437e476a025ade402d47343e91a10a4d
yasuyuki mitsui <mitsui.y.k@gmail.com>
2015-05-09 17:56:34 +0900

    画面遷移をfadeIn,fadeOutにするように変更

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,16 +1,16 @@
 /**
  * riot.routeの機能拡充
  */
-(function(){
+(function () {
   // ルーティングパースの変更
-  riot.route.parser(function(path) {
+  riot.route.parser(function (path) {
     var raw = path.split('?'),
       uri = [raw[0]],
       qs = raw[1],
       params = {}
 
     if (qs) {
-      qs.split('&').forEach(function(v) {
+      qs.split('&').forEach(function (v) {
         var c = v.split('=')
         params[c[0]] = c[1]
       })
@@ -24,19 +24,18 @@
   riot.observable(riot.route);
   var currentFragment = '';
   var currentParam = null;
-  var routes = function(fragment, param) {
+  var routes = function (fragment, param) {
     fragment = fragment || 'home'; // デフォルトはＨＯＭＥ画面
     var paths = fragment.split('/');
     var currentPaths = currentFragment.split('/');
     var routePath = '';
-    for(var i = 0; i< paths.length; i++){
+    for (var i = 0; i < paths.length; i++) {
       var nextPath = paths[i];
       var currentPath = currentPaths[i];
-      if(nextPath !== currentPath){
-        riot.route.trigger('routeChange'+(routePath === '' ? '' : ':')+routePath, nextPath);
+      if (nextPath !== currentPath) {
+        changeView(routePath, nextPath, param);
         routePath += (routePath === '' ? '' : '/') + nextPath;
-        riot.route.trigger(routePath, param);
-      } else if (!_.isEqual(param, currentParam)){
+      } else if (!_.isEqual(param, currentParam)) {
         routePath += (routePath === '' ? '' : '/') + nextPath;
         riot.route.trigger(routePath, param);
       } else {
@@ -47,8 +46,54 @@
     currentFragment = fragment;
     currentParam = param;
   };
+
+  /**
+   * 画面の切り替え
+   * fadeOut -> スクロール位置のリセット -> fadeIn
+   * @type {boolean}
+   */
+  var isAnimating = false;
+  var changeView = function(routePath, nextPath, param){
+    var $doc = $('body');
+    if(isAnimating){
+      riot.route.trigger('routeChange'+(routePath === '' ? '' : ':')+routePath, nextPath);
+      routePath += (routePath === '' ? '' : '/') + nextPath;
+      riot.route.trigger(routePath, param);
+    } else {
+      isAnimating = true;
+      $doc.fadeOut(100, function(){
+        riot.route.trigger('routeChange'+(routePath === '' ? '' : ':')+routePath, nextPath);
+        $doc.scrollTop(0);
+        $doc.fadeIn(function(){
+          isAnimating = false;
+        });
+        routePath += (routePath === '' ? '' : '/') + nextPath;
+        riot.route.trigger(routePath, param);
+      });
+    }
+  };
+
+  /**
+   * 指定したエレメントが画面に表示されたタイミングでハンドラーをコールします。
+   * @param event
+   * @param el
+   * @param handler
+   */
+  riot.route.onAttached = function(event, el, handler){
+    this.on(event, function(param){
+      el = $(el);
+      var checkVisible = setInterval(function(){
+        if(!el.length || !el.is(':visible')){
+          return;
+        }
+        clearInterval(checkVisible);
+        handler(param);
+      },50);
+    });
+  };
   riot.route(routes);
-  riot.route.build = function(){
+  riot.route.build = function () {
+    $('body').hide();
     this.exec(routes);
   };
 })();
