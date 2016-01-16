426f54d4b00ce0c2a4812c4a5b64dac04d213386
Mitsui Yasuyuki <mitsui.y.k@gmail.com>
2016-01-17 03:24:53 +0900

    天気予報APIをサーバーでコールしてhtmlに埋め込むように改修

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,29 @@ var express = require('express');
 var router = express.Router();
 var env = require('../app_server/env');
 var debug = require('debug')('feschibal:index');
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var weatherData = [{a:"b"}];
-  res.render('index', { weatherData: weatherData });
+  var url = env.url.weather.url;
+  var cityCode = env.url.weather.cityCode;
+  var apikey = process.env.APIKEY;
+
+  var path = url + "?code=" + cityCode + "&key=" + apikey;
+  debug("weather path:" + path);
+
+  var options = {
+    url: path,
+    json: true
+  };
+
+  request.get(options, function (error, response, body) {
+    var weatherData;
+    if(!error && body.status === 'success'){
+      weatherData = body;
+    }
+    res.render('index', { weatherData: weatherData });
+  });
 });
 
 module.exports = router;
